fc957bdf12fc133e92ef80af789e1f3c6c093936
tamanjic <tamanji.che@metropolia.fi>
2022-01-12 03:28:40 +0200

    closed db instance after test execution

--- src/tests/serverHealthCheck.test.ts
+++ src/tests/serverHealthCheck.test.ts
@@ -1,5 +1,6 @@
 import supertest from 'supertest';
 import app from '../app';
+import { sequelize } from '../utils/db';
 
 const api = supertest(app);
 
@@ -7,4 +8,8 @@ test('health check is ok; server is running', async () => {
   const serverStatus = await api.get('/health').expect(200);
 
   expect(serverStatus.text).toBe('ok');
+});
+
+afterAll(() => {
+  sequelize.close().catch((error) => console.log(error.message));
 });
